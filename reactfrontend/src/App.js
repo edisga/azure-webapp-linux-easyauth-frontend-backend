959f35c126de496fc00ded74a2225712ae66a868
Edison Garcia <egachi@live.com>
2020-03-16 19:34:44 -0500

    Updating

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import AuthService from './AuthService';
-import BackendAPI from './BackendAPI';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,23 +6,6 @@ class App extends Component {
   constructor(){
     super();
     this.state = { token: '', backend: '' };
-    
-    
-    // this.Auth = new AuthService();
-    // this.Auth.request()
-    //     .then(res =>{
-    //       //console.log(res);
-    //       this.BackendAPI = new BackendAPI();
-    //       this.BackendAPI.request(res.user_id)
-    //         .then(respBackend => {
-    //           this.respBackend = respBackend;
-    //         }).catch(errBackend =>{
-    //           console.log(errBackend);
-    //         });
-    //     })
-    //     .catch(err =>{
-    //       console.log(err);
-    //     })
   }
 
   async componentDidMount() {
@@ -35,16 +16,24 @@ class App extends Component {
       console.log('String: ' + jsonString);
       const jsonArray = JSON.parse(jsonString)
       console.log('Array: '+ jsonArray);
-      var id_token =jsonArray[0].id_token;
+      var id_token =jsonArray[0].id_token;  // or jsonArray[0].access_token
       console.log('Token: '+ id_token);
       this.setState({ token: jsonArray[0].id_token });
-      if (jsonArray!=undefined) {
+      if (jsonArray !== undefined) {
         console.log('Array has content');
-        fetch('https://www.w3.org/TR/PNG/iso_8859-1.txt').then(response => this.setState({ backend: response.data })).catch();
-        //let headers = { "Authorization": "Bearer " + id_token };
-        //fetch(url, { headers,options })
-         // .then(this._checkStatus)
-          //.then(response => response.json());
+        const requestToBackEnd = await fetch('https://edisga-nodejs-backend.azurewebsites.net/echo/test',
+        {
+          method: "GET",
+          //body: JSON.stringify(data),
+          headers: {
+            "Authorization": "Bearer " + id_token 
+          },
+          //credentials: "same-origin"
+        });
+        const responseFromBackEnd = await requestToBackEnd.json();
+        const jsonStringBackEnd = JSON.stringify(responseFromBackEnd)
+        this.setState({ backend: jsonStringBackEnd });
+        console.log('String Backend: ' + jsonStringBackEnd);
       }
     } catch (error) {
       console.log(error);
@@ -62,7 +51,7 @@ class App extends Component {
           Token from Azure AD: { this.state.token }
         </p>
         <p className="App-intro">
-          Values from Backed: { this.state.backend }
+          Values from Backend: { this.state.backend }
         </p>
       </div>
     );
